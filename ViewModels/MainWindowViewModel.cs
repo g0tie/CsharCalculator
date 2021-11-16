3b950c3f6d5a21c83bbb40c69d9f224dad42a0b3
g0tie <gaultierbegue@outlook.com>
2021-11-16 15:58:41 +0400

    first commit init

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpCalculator.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        private string _currentInput = "0";
+
+        public string CurrentInput 
+        {
+            get => _currentInput;
+            set {
+                _currentInput = value;
+            }
+        }
+    }
+}
