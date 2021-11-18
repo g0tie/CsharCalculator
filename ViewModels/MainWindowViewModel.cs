f422b86816732857f5fa4677480c10fa3beec652
g0tie <gaultierbegue@outlook.com>
2021-11-18 09:48:58 +0400

    adding sign inversion and parenthesis support

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -30,19 +30,32 @@ namespace CsharpCalculator.ViewModels
             
             int lastIndex = CurrentInput.Length - 1;
             char lastCharacter = CurrentInput[lastIndex];
+
             bool isLastCharNumber = int.TryParse(lastCharacter.ToString(), out nb);
             bool isCharToAddNumber = int.TryParse(character.ToString(), out nb);
 
-            if (!isLastCharNumber && !isCharToAddNumber) return;
-            
+            // interdire d'ajouter un signe après un autre 
+            if (lastCharacter != ')' && !isLastCharNumber && !isCharToAddNumber) return;
+
             CurrentInput += character;
         }
 
         public void ExecuteCalculation()
         {		
-            string[] calculationMembers = Regex.Split(CurrentInput,  @"[-+*\/()]");
-            MatchCollection operationsMatches = Regex.Matches(CurrentInput, @"[-+*\/()]");
-            var operationTypes = operationsMatches.Cast<Match>().Select(match => match.Value).ToList();
+            string[] calculationMembers = Regex.Split(CurrentInput,  @"[\+\*\/]|(?<!\()\-"); //Filtrer uniquement les nombres
+
+            Console.WriteLine(calculationMembers[0]);
+
+            for (int i = 0; i < calculationMembers.Length; i++) {
+
+                if (Regex.IsMatch(calculationMembers[i], @"()")) {
+                    calculationMembers[i] = parenthesisRemove(calculationMembers[i]);
+                }
+            }
+            Console.WriteLine(calculationMembers[0]);
+
+            MatchCollection operationsMatches = Regex.Matches(CurrentInput, @"[\+\*\/]|(?<!\()\-"); //Filtrer uniquement les signes
+            var operationTypes = operationsMatches.Cast<Match>().Select(match => match.Value).ToList(); //Convertir les signes en string
 
             double result = double.Parse(calculationMembers[0]);
 
@@ -52,6 +65,8 @@ namespace CsharpCalculator.ViewModels
 
                     switch (operationTypes[i]) {
                         case "+":
+
+                            // If number to add is a double, convert, else keep int
                             if (Regex.IsMatch(calculationMembers[i + 1], @"\," ))
                             {
                                 result += double.Parse(calculationMembers[i + 1]);
@@ -92,6 +107,8 @@ namespace CsharpCalculator.ViewModels
             }
 
             CurrentInput = Math.Round(result,3).ToString();
+
+            if (Regex.IsMatch(CurrentInput, @"[\-]")) CurrentInput = $"({CurrentInput})"; 
         }
 
 
@@ -103,6 +120,47 @@ namespace CsharpCalculator.ViewModels
         public void GoBack()
         {
             CurrentInput = CurrentInput.Remove(CurrentInput.Length - 1);
+        }
+
+        public void InvertSign()
+        {
+            string[] calculationMembers = Regex.Split(CurrentInput, @"[\+\*\/]|(?<!\()\-"); //Get string as array without sign operators
+
+            // Get last member
+            var lastCalcMember = calculationMembers[calculationMembers.Length - 1];
+            int lastMemberLen = lastCalcMember.Length;
+            
+            // check if last member in correct format
+            if (Regex.IsMatch(CurrentInput[CurrentInput.Length - 1].ToString(), @"[\+\*\/]|(?<!\()\-")) return;
+            if (Regex.IsMatch(lastCalcMember, @"[\-]")) lastCalcMember = parenthesisRemove(lastCalcMember);
+
+            int invertedLastMember = int.Parse(lastCalcMember) * (-1);
+            string invertedLastMemberFormatted = "";
+
+
+            //Get string version of inverted last calculation member 
+            if (invertedLastMember < 0) {
+                invertedLastMemberFormatted = $"({invertedLastMember.ToString()})";
+            } else {
+                invertedLastMemberFormatted = invertedLastMember.ToString();
+            }
+
+            // replace old member by new inverted one in calculation string 
+            if (calculationMembers.Length > 1) {
+
+                CurrentInput = CurrentInput.Remove(CurrentInput.Length - lastMemberLen, lastMemberLen);
+                CurrentInput = CurrentInput.Insert(CurrentInput.Length, invertedLastMemberFormatted);
+
+            } else {
+                // if contains just one number
+                CurrentInput = invertedLastMemberFormatted;
+            }
+           
+        }
+
+        static string parenthesisRemove(string nb)
+        {   
+            return Regex.Replace(nb, @"[()]", "");
         }
 
     }
