8996985ccac162f47773a0c33218c3c4a8550294
g0tie <gaultierbegue@outlook.com>
2021-11-19 19:13:31 +0400

    add license fucntionnality + devmode

--- ViewModels/LicenseKeyDialogViewModel.cs
+++ ViewModels/LicenseKeyDialogViewModel.cs
@@ -2,6 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using ReactiveUI;
+using System.Net.Http;
+using Newtonsoft.Json;
+using CsharpCalculator.Views;
+using System.IO;
+using Avalonia.Controls;
 
 namespace CsharpCalculator.ViewModels
 {
@@ -17,9 +22,54 @@ namespace CsharpCalculator.ViewModels
             }
         }
 
-        public void VerifyKey()
+        public void VerifyKey(Window window)
         {
+            bool isKeyConfirmed = getOnlineKey(LicenseKey);
+            string filePath = $"{Directory.GetCurrentDirectory()}/key.txt";
 
+            if (isKeyConfirmed) {
+                try
+                {
+                    File.WriteAllText(filePath, LicenseKey);
+                    MainWindow mainWIndow = new MainWindow
+                    {
+                        DataContext = new MainWindowViewModel(),
+                    };
+                    
+                    mainWIndow.Show();
+                    window.Close();
+                }
+                catch (Exception e) {
+                    Console.WriteLine(e);
+                }
+            }
+
+            window.FindControl<TextBlock>("Error").Text = "Wrong key !";
+        }
+
+        private bool getOnlineKey(string licensekey)
+        {
+            if (licensekey == "devmode") return true;
+
+            var client = new HttpClient();
+
+            var pairs = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("key", licensekey),
+            };
+
+            var content = new FormUrlEncodedContent(pairs);
+
+            var response = client.PostAsync("http://localhost:8000/api/verify", content).Result;
+            var responseStatus = JsonConvert.DeserializeObject<Dictionary<string, string>>(
+                response.Content.ReadAsStringAsync().Result
+            );
+
+            if (responseStatus == null || responseStatus["status"] != "200") {
+                return false;
+            } else {
+                return true;
+            }
         }
     }
 }
